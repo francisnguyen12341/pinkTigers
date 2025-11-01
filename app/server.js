39d9e09a652b2e7968dae4cf9181de91f18ce2ed
Vanessa Trinh <xtourmaline@gmail.com>
2025-11-01 19:47:57 -0400

    working create account
    want to do some error checking for if there is this account already. think i will do a select query for that ???

--- app/server.js
+++ app/server.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const pg = require("pg");
 const express = require("express");
 const app = express();
@@ -16,6 +18,29 @@ app.use(express.static("public"));
 app.use(express.json());
 
 // Add server endpoints here
+
+app.post("/create-account", (req, res) => {
+    let reqBody = req.body;
+
+    if ((reqBody.hasOwnProperty("username")) && (reqBody.hasOwnProperty("password"))) {
+        let userName = reqBody.username;
+        let passHash = crypto.createHash('sha256').update(reqBody.password).digest('hex');
+
+        pool.query(`INSERT INTO users (username, password) VALUES ($1, $2);`, [ userName, passHash ]).then(() => {
+            console.log(`Successfully added user: ${userName}`);
+            res.status(200);
+            res.send();
+        }).catch(err => {
+            console.log("Error inserting user:", err);
+            res.status(500)
+            res.send();
+        });
+    } else {
+        console.log("Missing username or password");
+        res.status(404);
+        res.send();
+    }
+});
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
