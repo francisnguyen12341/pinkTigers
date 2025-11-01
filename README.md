0acb35bdcf2a608ea1efdeebfe599e09dfa3a56a
root <root@DESKTOP-K0ADN1R.localdomain>
2025-11-01 17:22:52 -0400

    Initial project setup commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pinkTigers
 Git Repository for Pink Tigers Webpage Project
+
+# Setup
+### Installing Packages
+```bash
+npm install
+```
+
+### Create Database
+1. Add your postgres password to env.json
+2. If needed, update package.json scripts depending on your environment
+3. Run the following command:
+```bash
+npm run setup
+```
+
+### Drop Database
+```bash
+npm run drop
+```
+
+### Run Server
+```bash
+npm run start
+```
---
+++ setup.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE obsidianclone;
+\c obsidianclone
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  main_folder_id INTEGER
+);
+
+CREATE TABLE folders (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  parent_id INTEGER REFERENCES folders(id) ON DELETE CASCADE
+);
+
+CREATE TABLE notes (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  text VARCHAR(10000),
+  folder_id INTEGER NOT NULL REFERENCES folders(id) ON DELETE CASCADE
+);
+
+ALTER TABLE users
+  ADD CONSTRAINT fk_main_folder
+  FOREIGN KEY (main_folder_id) REFERENCES folders(id);
