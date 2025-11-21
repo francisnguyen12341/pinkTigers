f2770028cda24c762a13834451fc6f79ee7511eb
Vanessa Trinh <xtourmaline@gmail.com>
2025-11-21 01:06:12 -0500

    this is broken :(

--- app/api/folders.js
+++ app/api/folders.js
@@ -1,57 +1,72 @@
 const pool = require("../utils/connection");
 const app = require("express").Router();
+const auth = require("../utils/auth");
 
-app.get("/get-user-folders", (req, res) => {
-    let username = req.query.username;
+app.get("/get-user-folders", auth, (req, res) => {
+    let { token } = req.cookies;
+    
+    let username;
+    let mainFolderID;
 
-    if (!username) {
-        res.json({ error: "Username is required" });
-        res.status(400);
-        return;
-    }
+    pool.query(`SELECT username FROM tokens WHERE token = $1`, [token]).then((result) => {
+        if (result.rows.length > 0) {
+           username = result.rows[0].username;
+           console.log("Found username: " + username);
+        }
+    }).catch((error) => {
+        console.log(error);
+        res.status(500);
+        res.send();
+    });
 
-    // plan here: query for the user's folders whenever we figure out how notes are implemented and then basically send back the whole folder system
-    pool.query('SELECT id FROM users WHERE username = $1;', [username])
-        .then(result => {
-            if (result.rows.length > 0) {
-                let userId = result.rows[0].id;
+    pool.query(`SELECT main_folder_id FROM users WHERE username = $1`, [username]).then((result) => {
+        console.log(result.rows);
 
-                let userInfo = {
-                    userName: username,
-                    userId: userId,
-                    // this part will prob need to be change
-                    folders: {
-                        "folderName": "RootFolder",
-                        "folders": [
-                            {
-                                "folderName": "Subfolder1",
-                                "folders": []
-                            },
-                            {
-                                "folderName": "Subfolder2",
-                                "folders": []
-                            },
-                            {
-                                "folderName": "Subfolder3",
-                                "folders": []
-                            }
-                        ]
-                    }
-                };
+        if (result.rows.length > 0) {
+            mainFolderID = result.rows[0].main_folder_id;
+            console.log("Found mainFolderID: " + mainFolderID);
+        }
+    }).catch((error) => {
+        console.log(error);
+        res.status(500);
+        res.send();
+    });
 
-                res.status(200);
-                res.setHeader("Content-Type", "text/json");
-                res.json(userInfo);
-            } else {
-                res.status(404);
-                res.json({ error: "User not found" });
+    let mainFolderName;
+
+    pool.query(`SELECT name FROM folders WHERE id = $1;`, [mainFolderID]).then(result => {
+        mainFolderName = result.rows[0].name;
+        console.log("Found mainFolderName: " + mainFolderName);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500);
+        res.send();
+    });
+
+    let recursiveSearch = (folder_id) => {
+        let folders = [];
+
+        pool.query(`SELECT (id, name) FROM folders WHERE parent_id = $1;`, [folder_id]).then(result => {
+            for (let row of result.rows) {
+                folders.push({
+                    folderName: row.name,
+                    folders: recursiveSearch(row.id),
+                });
             }
-        })
-        .catch(err => {
-            console.error("Error querying user:", err);
+        }).catch((error) => {
+            console.log(error);
             res.status(500);
-            res.json({ error: "Server error" });
+            res.send();
         });
+
+        return folders;
+    };
+
+    res.status(200);
+    res.json({
+        username,
+        folders: recursiveSearch(mainFolderID),
+    });
 });
 
 app.get("/folder", (req, res) => {
--- app/api/index.js
+++ app/api/index.js
@@ -2,12 +2,10 @@ const router = require("express").Router();
 
 const folders = require("./folders.js");
 const notes = require("./notes.js");
-const tokens = require("./tokens.js");
 const users = require("./users.js");
 
 router.use("/", folders);
 router.use("/", notes);
-//router.use("/", tokens);
 router.use("/", users);
 
 module.exports = router;
