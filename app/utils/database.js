19e6663fa088ef4966f94107f0d7940fe594e1d9
Francis <ftn23@drexel.edu>
2025-11-30 02:03:22 -0500

    so vercel and railway have an integration system that works between the two of them to share environment variables. that is what process.env is doing

--- app/utils/database.js
+++ app/utils/database.js
@@ -3,7 +3,7 @@ const pg = require("pg");
 
 const Pool = pg.Pool;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_PUBLIC_URL,
   ssl: { rejectUnauthorized: false }
 });
 
